0e8e14d531932b7f47a6cdcfbcbc7e138f31948b
f92esmup <f92esmup@gmail.com>
2025-09-30 13:51:29 +0200

    He corregido el nombre de un directorio y estoy probando brevemente la cli de producción. Y la configuración

--- live.py
+++ live.py
@@ -1,1 +1,23 @@
 """Orquestador del modo LIVE / TESTNET"""
+
+import logging
+
+from src.AdqusicionDatos.utils.logger import setup_logger
+
+from src.produccion import ProductionConfig, parse_args
+
+# Configurar logger
+setup_logger()
+log: logging.Logger = logging.getLogger(f"AFML.{__name__}")
+
+
+def main() -> None:
+    """Función para ejecutar el proceso principal"""
+    print("inciando modo LIVE / TESTNET")
+    args = parse_args()
+
+    print(args.train_id)
+
+
+if "__name__" == "__main__":
+    main()
--- src/produccion/config/config.py
+++ src/produccion/config/config.py
@@ -1,6 +1,7 @@
 """Configuración de la aplicación de producción"""
 
 from pydantic import BaseModel, Field
+from typing import List
 import argparse
 import yaml
 import logging
@@ -11,6 +12,30 @@ log = logging.getLogger("config_logger")
 ##########################################################################################################
 # Clases que descomponen los parámetros de configuración.
 ##########################################################################################################
+
+
+class MarketStats(BaseModel):
+    """Estadísticas de normalización del mercado."""
+
+    count: int
+    mean: List[List[float]]
+    var: List[List[float]]
+
+
+class PortfolioStats(BaseModel):
+    """Estadísticas de normalización del portafolio."""
+
+    count: int
+    mean: List[float]
+    var: List[float]
+
+
+class ObsNorm(BaseModel):
+    """Parámetros de normalización de las observaciones."""
+
+    _clip_obs: float
+    market: MarketStats
+    portfolio: PortfolioStats
 
 
 ##########################################################################################################
@@ -26,17 +51,22 @@ class ProductionConfig(BaseModel):
     simbolo: str = Field(..., description="Símbolo del activo a analizar")
     train_id: str = Field(..., description="Identificador del entrenamiento")
     model_path: str = Field(..., description="Ruta al modelo entrenado")
+    obs_norm: ObsNorm = Field(
+        ..., description="Parámetros de normalización de observaciones"
+    )
 
     @classmethod
     def load_config(cls, args: argparse.Namespace) -> "ProductionConfig":
         """Carga la configuración desde los argumentos de línea de comandos"""
 
+        config_path = f"entrenamientos/{args.train_id}/config_metadata.yaml"
+
         try:
-            with open(args.config.file, "r", encoding="utf-8") as file:
+            with open(config_path, "r", encoding="utf-8") as file:
                 yaml_data = yaml.safe_load(file)
         except FileNotFoundError:
             raise FileNotFoundError(
-                f"No se encontró el archivo de configuración: {arg.config.file}"
+                f"No se encontró el archivo de configuración: {args.config.file}"
             )
         except yaml.YAMLError as e:
             raise ValueError(f"Error al analizar el archivo YAML: {e}")
@@ -51,7 +81,7 @@ class ProductionConfig(BaseModel):
                 "simbolo": yaml_data.get("Datasets").get("symbol"),
                 "train_id": args.train_id,
                 "model_path": yaml_data.get("Output").get("model_path"),
-                # OBS NORM FALTA POR AÑADIR
+                "obs_norm": yaml_data.get("obs_norm"),
             }
         except AttributeError as e:
             log.error(f"Error al extraer parámetros de configuración: {e}")
