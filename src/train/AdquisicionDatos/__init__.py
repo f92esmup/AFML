35d4bf2b1f7fdfbb07415457e5a9c4d7706c75a7
f92esmup <f92esmup@gmail.com>
2025-10-04 00:32:01 +0200

    refactor: eliminar pipeline.py y actualizar exports
    - Eliminar DataPipeline (funcionalidad integrada en train.py)
    - Actualizar __init__.py para exportar DataDownloader y Preprocesamiento
    - Añadir nota de deprecación en docstring

--- src/train/AdquisicionDatos/__init__.py
+++ src/train/AdquisicionDatos/__init__.py
@@ -3,11 +3,17 @@ AFML - Adquisición de Datos para el Trading Financiero Automatizado.
 
 Este sub-módulo es responsable de la recopilación y procesamiento de datos
 financieros relevantes para el sistema de trading.
+
+Nota: DataPipeline ha sido deprecado. La funcionalidad de descarga y preprocesamiento
+está ahora integrada directamente en train.py mediante los componentes individuales
+DataDownloader y Preprocesamiento.
 """
-from src.train.AdquisicionDatos.pipeline import DataPipeline
+from src.train.AdquisicionDatos.adquisicion import DataDownloader
+from src.train.AdquisicionDatos.preprocesamiento import Preprocesamiento
 from src.train.AdquisicionDatos.utils.logger import setup_logger
 
 __all__ = [
-    "DataPipeline",
+    "DataDownloader",
+    "Preprocesamiento",
     "setup_logger",
 ]
